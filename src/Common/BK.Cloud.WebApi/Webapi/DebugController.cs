6034eadc5bf7d7e15274a3585d907d7385e133be
tanlingde <tan8864321@163.com>
2022-05-19 16:44:04 +0800

    提交测试新增新代码

---
+++ src/Common/BK.Cloud.WebApi/Webapi/DebugController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using BK.Cloud.Facade;
+using BK.Cloud.Model.Customer;
+using BK.Cloud.Model.Data.Model;
+using BK.Cloud.Web.App_Start;
+
+namespace BK.Cloud.WebApi.Webapi
+{
+
+    public class DebugController : BaseApi
+    {
+        [HttpGet]
+        [HttpPost]
+        [LoginCheckFiter(false)]
+        public dynamic AddOrg([FromBody] TB_DebugLogMetadata Obj)
+        {
+            if (Obj == null)
+            {
+                Obj = new TB_DebugLogMetadata();
+            }
+            Obj = SetObjectVal(Obj);
+
+            return Json(WebQuery.Instance.InsertObj(ObjToCondition(Obj, "TB_Debug")));
+        }
+    }
+}
